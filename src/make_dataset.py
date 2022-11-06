9edc359697805690e59a8bc429efa2138cc3dee6
hdeep03 <35414693+hdeep03@users.noreply.github.com>
2022-11-05 23:23:53 -0400

    Added Whisper Transcription Code

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,14 +1,17 @@
 """
-Youtube Video Batch Downloader
+Build Dataset
 
 @author Harsh Deep
 """
 
 import readline
+import json
+import os
 import subprocess
 import argparse
 from multiprocessing import Pool
 import time
+import whisper
 
 MAX_THREADS = 4
 
@@ -20,14 +23,26 @@ def yt_downloader(url):
     """
     subprocess.run(["yt-dlp", "-o", "data/raw/%(id)s.%(ext)s", "-f", "mp4", url])
 
+def audioTranscribe(file, target_file):
+    '''
+    Transcribes the audio from a file
+    '''
+    if ".DS" in file:
+        return 0;
+    #print('Loading whisper model ...')
+    model = whisper.load_model("base")
+    #print('Model Loaded')
+    result = model.transcribe(file)
+    with open(f"{target_file}.json", "w") as outfile:
+        outfile.write(json.dumps(result['segments']))
+
 
 if __name__ == '__main__':
-
     start = time.time()
     parser = argparse.ArgumentParser(description='Batch Downloader for Youtube Videos')
     parser.add_argument("file", help = "Input file with list of YT urls")
     path = parser.parse_args()
-    
+    '''
     with open(path.file, 'r') as f:
         urls = f.readlines()
         print(f'{len(urls)} videos to download')
@@ -35,5 +50,9 @@ if __name__ == '__main__':
         with Pool(workers) as p:
             print(f'Downloading with {workers} workers...')
             p.map(yt_downloader, urls)
-    print(f'Task completed in {time.time()-start:.2f} seconds')
-            
+        print(f'Download completed in {time.time()-start:.2f} seconds')
+    '''
+    print('Beginning Audio Transcription ...')
+    for f in os.listdir("data/raw/"):
+        audioTranscribe('data/raw/{}.mp4'.format(f[:-4]), 'data/processed/{}'.format(f[:-4])) 
+    
